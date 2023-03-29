b987361cd03cd3810fcae21796b14e5dd09fb866
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-03-29 15:52:30 -0400

    Updates to coin flipper

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,17 +46,18 @@ document.querySelector('#flip').addEventListener('click', function() {
 
     
     document.querySelector('#clear').addEventListener('click', function() {
-        let headsFlips = 0
-        let tailsFlips = 0
-        let headsPercent = '0%'
-        let tailsPercent = '0%'
-        let totalFlips = 0
+        headsFlips = 0
+        tailsFlips = 0
+        headsPercent = '0%'
+        tailsPercent = '0%'
+        totalFlips = 0
         document.querySelector('#penny-image').src = 'assets/images/penny-heads.jpg'
         document.querySelector('#message').textContent = "Let's Get Flipping!"
         document.querySelector('#heads').textContent = headsFlips
         document.querySelector('#tails').textContent = tailsFlips
         document.querySelector('#heads-percent').textContent = headsPercent
         document.querySelector('#tails-percent').textContent = tailsPercent
+        
     })
 
     })
